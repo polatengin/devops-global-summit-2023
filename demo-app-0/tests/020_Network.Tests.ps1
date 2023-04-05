c5c8d395db67db8bdfa35d14734813225367cb2b
Engin Polat <enpolat@microsoft.com>
2023-04-04 18:41:05 -0700

    adding initial network test scripts

---
+++ demo-app-0/tests/020_Network.Tests.ps1
@@ -0,0 +1,65 @@
+BeforeAll {
+  Import-Module ../../BenchPress/BenchPress.Azure.psd1
+}
+
+Describe 'Verify Network Layer' {
+  it 'Should contain the Storage Account' {
+    #arrange
+    $rgName = "$env:RESOURCE_GROUP_NAME"
+    $name = "nwstrg$env:RESOURCE_SUFFIX"
+
+    #act
+    $result = Confirm-AzBPStorageAccount -Name "$name" -ResourceGroupName "$rgName"
+
+    #assert
+    $result | Should -BeSuccessful
+  }
+
+  it 'Should contain the Storage Account in Standard_LRS Sku' {
+    #arrange
+    $rgName = "$env:RESOURCE_GROUP_NAME"
+    $name = "nwstrg$env:RESOURCE_SUFFIX"
+
+    #act
+    $result = Confirm-AzBPStorageAccount -Name "$name" -ResourceGroupName "$rgName"
+
+    #assert
+    $result.ResourceDetails.Sku.Name | Should -Be "Standard_LRS"
+  }
+
+  it 'Should contain the Storage Account in Hot Access Tier' {
+    #arrange
+    $rgName = "$env:RESOURCE_GROUP_NAME"
+    $name = "nwstrg$env:RESOURCE_SUFFIX"
+
+    #act
+    $result = Confirm-AzBPStorageAccount -Name "$name" -ResourceGroupName "$rgName"
+
+    #assert
+    $result.ResourceDetails.AccessTier | Should -Be "Hot"
+  }
+
+  it 'Should contain the Storage Account https traffic only' {
+    #arrange
+    $rgName = "$env:RESOURCE_GROUP_NAME"
+    $name = "nwstrg$env:RESOURCE_SUFFIX"
+
+    #act
+    $result = Confirm-AzBPStorageAccount -Name "$name" -ResourceGroupName "$rgName"
+
+    #assert
+    $result.ResourceDetails.EnableHttpsTrafficOnly | Should -Be $true
+  }
+
+  it 'Should contain the Storage Account minimum tls version TLS1.2' {
+    #arrange
+    $rgName = "$env:RESOURCE_GROUP_NAME"
+    $name = "nwstrg$env:RESOURCE_SUFFIX"
+
+    #act
+    $result = Confirm-AzBPStorageAccount -Name "$name" -ResourceGroupName "$rgName"
+
+    #assert
+    $result.ResourceDetails.MinimumTlsVersion | Should -Be "TLS1_2"
+  }
+}
