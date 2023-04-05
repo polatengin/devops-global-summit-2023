0192e0e8e08116c1b94e287bc946132a7b9c0403
Engin Polat <enpolat@microsoft.com>
2023-04-04 18:41:39 -0700

    adding initial cosmos test scripts

---
+++ demo-app-0/tests/050_Cosmos.ps1
@@ -0,0 +1,11 @@
+BeforeAll {
+  Import-Module ../../BenchPress/BenchPress.Azure.psd1
+}
+
+Describe 'Verify Cosmos layer' {
+  it 'Should contain the resource' {
+    #arrange
+    #act
+    #assert
+  }
+}
