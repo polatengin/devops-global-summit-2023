186bf172e1c02c46727bc6964903cb9f35a1465c
Engin Polat <enpolat@microsoft.com>
2023-04-04 18:43:41 -0700

    adding initial test scripts

---
+++ demo-app-1/DemoApp.Tests.ps1
@@ -0,0 +1,110 @@
+BeforeAll {
+  Import-Module "../BenchPress/BenchPress.Azure.psd1"
+}
+
+Describe 'Resource Group Tests' {
+  it 'Should contain a resource group with the given name' {
+    #arrange
+    $rgName = "benchpress-rg-${env:ENVIRONMENT_SUFFIX}"
+
+    #act
+    $result = Confirm-AzBPResourceGroup -ResourceGroupName $rgName
+
+    #assert
+    $result | Should -BeSuccessful
+  }
+}
+
+Describe 'Service Plan Tests' {
+  it 'Should contain a service plan with the given name' {
+    #arrange
+    $rgName = "benchpress-rg-${env:ENVIRONMENT_SUFFIX}"
+    $svcPlanName = "benchpress-hosting-plan-${env:ENVIRONMENT_SUFFIX}"
+
+    #act
+    $result = Confirm-AzBPAppServicePlan -ResourceGroupName $rgName -AppServicePlanName $svcPlanName
+
+    #assert
+    $result | Should -BeSuccessful
+  }
+}
+
+Describe 'Action Group Tests' {
+  it 'Should contain an email action group with the given name' {
+    #arrange
+    $rgName = "benchpress-rg-${env:ENVIRONMENT_SUFFIX}"
+    $agName = "benchpress-email-action-group-${env:ENVIRONMENT_SUFFIX}"
+
+    #act
+    $result = Confirm-AzBPActionGroup -ResourceGroupName $rgName -ActionGroupName $agName
+
+    #assert
+    $result | Should -BeSuccessful
+  }
+}
+
+Describe 'Web Apps Tests' {
+  it 'Should contain a web app with the given name' {
+    #arrange
+    $webAppName = "benchpress-web-${env:ENVIRONMENT_SUFFIX}"
+    $rgName = "benchpress-rg-${env:ENVIRONMENT_SUFFIX}"
+
+    #act
+    $result = Confirm-AzBPWebApp -ResourceGroupName $rgName -WebAppName $webAppName
+
+    #assert
+    $result | Should -BeSuccessful
+  }
+
+  it 'Should have the web app availability state as normal' {
+    #arrange
+    $webAppName = "benchpress-web-${env:ENVIRONMENT_SUFFIX}"
+    $rgName = "benchpress-rg-${env:ENVIRONMENT_SUFFIX}"
+
+    #act
+    $result = Confirm-AzBPWebApp -ResourceGroupName $rgName -WebAppName $webAppName
+
+    #assert
+    $result.ResourceDetails.AvailabilityState | Should -Be "Normal"
+  }
+
+  it 'Should have the web app works https only' {
+    #arrange
+    $webAppName = "benchpress-web-${env:ENVIRONMENT_SUFFIX}"
+    $rgName = "benchpress-rg-${env:ENVIRONMENT_SUFFIX}"
+
+    #act
+    $result = Confirm-AzBPWebApp -ResourceGroupName $rgName -WebAppName $webAppName
+
+    #assert
+    $result.ResourceDetails.HttpsOnly | Should -Be $true
+  }
+
+  it 'Should contain configuration in the web app' {
+    #arrange
+    $webAppName = "benchpress-web-${env:ENVIRONMENT_SUFFIX}"
+    $rgName = "benchpress-rg-${env:ENVIRONMENT_SUFFIX}"
+
+    #act
+    $result = Confirm-AzBPWebApp -ResourceGroupName $rgName -WebAppName $webAppName
+
+    #assert
+    $result.ResourceDetails.SiteConfig.AppSettings | Should -Be -Not $null
+  }
+
+  it 'Should running' {
+    #arrange
+    $webAppName = "benchpress-web-${env:ENVIRONMENT_SUFFIX}"
+    $rgName = "benchpress-rg-${env:ENVIRONMENT_SUFFIX}"
+
+    #act
+    $result = Confirm-AzBPWebApp -ResourceGroupName $rgName -WebAppName $webAppName
+
+    #assert
+    $result.ResourceDetails.State | Should -Be "Running"
+  }
+}
+
+AfterAll {
+  Get-Module BenchPress.Azure | Remove-Module
+}
