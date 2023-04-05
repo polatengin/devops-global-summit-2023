e1e6e85c486a15a84fda816e84339745dca3798b
Engin Polat <enpolat@microsoft.com>
2023-04-04 18:41:32 -0700

    adding initial keyvault test scripts

---
+++ demo-app-0/tests/040_KeyVault.Tests.ps1
@@ -0,0 +1,65 @@
+BeforeAll {
+  Import-Module ../../BenchPress/BenchPress.Azure.psd1
+}
+
+Describe 'Verify KeyVault layer' {
+  it 'Should contain the KeyVault' {
+    #arrange
+    $rgName = "$env:RESOURCE_GROUP_NAME"
+    $name = "keyvault-$env:RESOURCE_SUFFIX"
+
+    #act
+    $result = Confirm-AzBPKeyVault -ResourceGroupName "$rgName" -Name "$name"
+
+    #assert
+    $result | Should -BeSuccessful
+  }
+
+  it 'Should contain the KeyVault with Standard Sku' {
+    #arrange
+    $rgName = "$env:RESOURCE_GROUP_NAME"
+    $name = "keyvault-$env:RESOURCE_SUFFIX"
+
+    #act
+    $result = Confirm-AzBPKeyVault -ResourceGroupName "$rgName" -Name "$name"
+
+    #assert
+    $result.ResourceDetails.Sku | Should -Be "Standard"
+  }
+
+  it 'Should contain the KeyVault with soft delete enabled' {
+    #arrange
+    $rgName = "$env:RESOURCE_GROUP_NAME"
+    $name = "keyvault-$env:RESOURCE_SUFFIX"
+
+    #act
+    $result = Confirm-AzBPKeyVault -ResourceGroupName "$rgName" -Name "$name"
+
+    #assert
+    $result.ResourceDetails.EnableSoftDelete | Should -Be $true
+  }
+
+  it 'Should contain the KeyVault with purge protection disabled' {
+    #arrange
+    $rgName = "$env:RESOURCE_GROUP_NAME"
+    $name = "keyvault-$env:RESOURCE_SUFFIX"
+
+    #act
+    $result = Confirm-AzBPKeyVault -ResourceGroupName "$rgName" -Name "$name"
+
+    #assert
+    $result.ResourceDetails.EnablePurgeProtection | Should -Be $null
+  }
+
+  it 'Should contain the KeyVault with public network access enabled' {
+    #arrange
+    $rgName = "$env:RESOURCE_GROUP_NAME"
+    $name = "keyvault-$env:RESOURCE_SUFFIX"
+
+    #act
+    $result = Confirm-AzBPKeyVault -ResourceGroupName "$rgName" -Name "$name"
+
+    #assert
+    $result.ResourceDetails.PublicNetworkAccess | Should -Be "Enabled"
+  }
+}
