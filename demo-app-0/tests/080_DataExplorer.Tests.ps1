6a1ca124db25a43e809c5f9f0bbe95077ad7be5f
Engin Polat <enpolat@microsoft.com>
2023-04-04 18:42:09 -0700

    adding initial data explorer test scripts

---
+++ demo-app-0/tests/080_DataExplorer.Tests.ps1
@@ -0,0 +1,11 @@
+BeforeAll {
+  Import-Module ../../BenchPress/BenchPress.Azure.psd1
+}
+
+Describe 'Verify Data Explorer layer' {
+  it 'Should contain the resource' {
+    #arrange
+    #act
+    #assert
+  }
+}
