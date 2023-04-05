75a13abd331d9a21ebea76297863bde2ee6d85e2
Engin Polat <enpolat@microsoft.com>
2023-04-04 18:42:01 -0700

    adding initial virtual machine test scripts

---
+++ demo-app-0/tests/070_VirtualMachine.Tests.ps1
@@ -0,0 +1,29 @@
+BeforeAll {
+  Import-Module ../../BenchPress/BenchPress.Azure.psd1
+}
+
+Describe 'Verify Virtual Machine layer' {
+  it 'Should contain the virtual machine' {
+    #arrange
+    $rgName = "$env:RESOURCE_GROUP_NAME"
+    $name = "vm-$env:RESOURCE_SUFFIX"
+
+    #act
+    $result = Confirm-AzBPVirtualMachine -ResourceGroupName "$rgName" -VirtualMachineName "$name"
+
+    #assert
+    $result | Should -BeSuccessful
+  }
+
+  it 'Should contain the virtual machines with Standard_D1_v2 VmSize' {
+    #arrange
+    $rgName = "$env:RESOURCE_GROUP_NAME"
+    $name = "vm-$env:RESOURCE_SUFFIX"
+
+    #act
+    $result = Confirm-AzBPVirtualMachine -ResourceGroupName "$rgName" -VirtualMachineName "$name"
+
+    #assert
+    $result.ResourceDetails.HardwareProfile.VmSize | Should -Be "Standard_D1_v2"
+  }
+}
